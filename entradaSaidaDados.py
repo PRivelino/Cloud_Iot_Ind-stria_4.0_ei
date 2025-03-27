0662163649e8df40eda99207ac3bab9951ed2a5f
UniFanor <UniFanor@FNS-WS-039503.EDUC.ACAD>
2025-03-27 20:54:03 -0300

    entradas e saidas de dados

---
+++ entradaSaidaDados.py
@@ -0,0 +1,13 @@
+numero1 = int(input('Digite um numero:'))
+numero2 = int(input('Digite outro numero:'))
+
+# Operadores aritméticos
+# Adição
+
+soma = numero1 + numero2
+print(soma)
+subtração = numero1 + numero2
+print(subtração)
+# Divisão
+divisão = numero1/numero2
+print(divisão)
