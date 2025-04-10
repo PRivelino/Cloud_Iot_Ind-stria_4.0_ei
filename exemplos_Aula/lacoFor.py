c2264a058f4690a5e4cf8cc0057c7b2250ea236d
UniFanor <UniFanor@FNS-WS-039503.EDUC.ACAD>
2025-04-10 20:43:45 -0300

    laco for

---
+++ exemplos_Aula/lacoFor.py
@@ -0,0 +1,12 @@
+
+
+
+
+# for variavel in range (10):
+#     print('Ola, mundo')
+lista = ['Carlos', 'Luka', 'Paulo', 'Erik','Adler','Rivelino', 'Jose',' Davi', 'Vitor']
+#for nome in lista:
+#    print(nome)
+
+# print(lista[0])
+print(lista[3])
